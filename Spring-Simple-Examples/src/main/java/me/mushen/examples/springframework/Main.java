4604f1074004eaa0b3518a0e8fae8a179ab13bc2
Remilia <HinanawiCirno@outlook.com>
2017-12-23 15:28:11 +0800

    init project

---
+++ Spring-Simple-Examples/src/main/java/me/mushen/examples/springframework/Main.java
@@ -0,0 +1,13 @@
+package me.mushen.examples.springframework;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @Desc
+ * @Author Remilia
+ * @Create 2017-12-23
+ */
+public class Main {
+    private static final Logger LOGGER = LoggerFactory.getLogger(Main.class);
+}
